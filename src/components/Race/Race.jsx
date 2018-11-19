63783dd1c152d9fa41936f8e8e27481885b5edb2
Arun Seehra <seehra.a@gmail.com>
2018-11-19 14:07:52 -0600

    Surround a Race with an anchor & fix Race display
    Allow us to eventually use a react router to route to the right place. Also
    tidy up the display to be closer to "polished" form

--- src/components/Race/Race.jsx
+++ src/components/Race/Race.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './Race.scss';
 import Card from '../Card';
 
 export default function Race(props) {
   const { id, location, name } = props;
   return (
-    <Card>
-      {id}
-      {name}
-      {location}
-    </Card>
+    <a href={`/${id}`} className="unadorned-link Race">
+      <Card>
+        <header>
+          <h2 className="h6 Race__name">{name}</h2>
+        </header>
+        <div className="Race__location">{location}</div>
+      </Card>
+    </a>
   );
 }
 
