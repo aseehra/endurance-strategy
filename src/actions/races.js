057a8d48ccdc1917570c9ad9033bb26e1eeae1ac
Arun Seehra <seehra.a@gmail.com>
2018-11-19 13:28:03 -0600

    Extract normalize errors to own utility function
    It will be used a fair bit, so why repeat code.

--- src/actions/races.js
+++ src/actions/races.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 
 import { API_BASE_URL } from '../config';
-import { ApiError, FetchError } from '../errors';
 import { FETCH_RACES, FETCH_RACES_FAILURE, FETCH_RACES_SUCCESS } from './types';
+import { normalizeErrors } from '../utils/fetch';
 
 const fetchRacesStart = () => ({
   type: FETCH_RACES,
@@ -22,17 +22,8 @@ export const fetchRaces = () => (dispatch) => {
   dispatch(fetchRacesStart());
 
   fetch(`${API_BASE_URL}/api/races`)
-    .then((res) => {
-      if (!res.ok) {
-        const contentType = res.headers.get('content-type');
-        if (contentType && contentType.startsWith('application/json')) {
-          return res.json().then(data => Promise.reject(new ApiError(data.error)));
-        }
-        return Promise.reject(new FetchError(res));
-      }
-
-      return res.json();
-    })
+    .then(normalizeErrors)
+    .then(res => res.json())
     .then(data => dispatch(fetchRacesSuccess(data.races)))
     .catch(error => dispatch(fetchRacesFailure(error)));
 };
---
+++ src/utils/fetch.js
@@ -0,0 +1,14 @@
+import { ApiError, FetchError } from '../errors';
+
+// eslint-disable-next-line import/prefer-default-export
+export function normalizeErrors(res) {
+  if (!res.ok) {
+    const contentType = res.headers.get('content-type');
+    if (contentType && contentType.startsWith('application/json')) {
+      return res.json().then(data => Promise.reject(new ApiError(data.error)));
+    }
+    return Promise.reject(new FetchError(res));
+  }
+
+  return res;
+}
