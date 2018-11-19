424e876d210a51990538bcad3a259d68a1dd78d0
Arun Seehra <seehra.a@gmail.com>
2018-11-19 15:12:30 -0600

    Add react-router and switch from anchors to Links

--- src/components/Race/Race.jsx
+++ src/components/Race/Race.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './Race.scss';
 import Card from '../Card';
@@ -7,14 +8,14 @@ import Card from '../Card';
 export default function Race(props) {
   const { id, location, name } = props;
   return (
-    <a href={`/${id}`} className="unadorned-link Race">
+    <Link to={`/${id}`} className="unadorned-link Race">
       <Card>
         <header>
           <h2 className="h6 Race__name">{name}</h2>
         </header>
         <div className="Race__location">{location}</div>
       </Card>
-    </a>
+    </Link>
   );
 }
 
--- src/containers/EnduranceStrategy/EnduranceStrategy.jsx
+++ src/containers/EnduranceStrategy/EnduranceStrategy.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import RaceList from '../RaceList';
 
 export default function EnduranceStrategy() {
   return (
-    <div className="root">
-      <header className="page-header">
-        <h1 className="h5">Endurance Strategy Reporter</h1>
-      </header>
-
-      <main>
-        <RaceList />
-      </main>
-    </div>
+    <Router>
+      <div className="root">
+        <header className="page-header">
+          <h1 className="h5">Endurance Strategy Reporter</h1>
+        </header>
+        <main>
+          <RaceList />
+        </main>
+      </div>
+    </Router>
   );
 }
