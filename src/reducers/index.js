e3095a7e67ecd80b1f9c0de5d7f7c513ae583d5a
Arun Seehra <seehra.a@gmail.com>
2018-11-26 11:33:18 -0600

    Make PageBanner display dynamic information
    Also lay the groundwork for fixing adding a secondary header

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,7 +9,9 @@ export default combineReducers({
 });
 
 export const getRaces = state => racesSelectors.getRaces(state.races);
+export const getRace = state => racesSelectors.getRace(state.races);
 export const getRaceEntries = state => racesSelectors.getRaceEntries(state.races);
+export const getRaceEntry = state => racesSelectors.getRaceEntry(state.races);
 export const getRacesLoading = state => racesSelectors.getRacesLoading(state.races);
 export const getEntriesLoading = state => racesSelectors.getEntriesLoading(state.races);
 export const getRaceEntriesFilter = state => racesSelectors.getFilter(state.races);
