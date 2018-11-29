ecc3654ebe3378e646376a20d32a31325ab2778e
Arun Seehra <seehra.a@gmail.com>
2018-11-29 15:09:51 -0600

    Rudimentary styling for NotFound page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './NotFound.scss';
+
 export default function NotFound() {
   return (
     <div className="NotFound">
       <header>
-        <h2>Error</h2>
+        <h2 className="NotFound__header">Error</h2>
       </header>
       <p>Oh no, we couldn&apos;t find what you were looking for.</p>
 
-      <Link to="/">Return home</Link>
+      <Link to="/" className="NotFound__link">
+        Return home
+      </Link>
     </div>
   );
 }
