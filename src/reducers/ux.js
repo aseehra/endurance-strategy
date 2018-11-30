d82a2ec043e4da0da81f759f65c312b969e3edbe
Arun Seehra <seehra.a@gmail.com>
2018-11-30 13:41:12 -0600

    Add state and reducers for isMobile
    We will have a thunk-action that will encapsulate actually setting isMobile
    based on a window width, called receiveWindowSize.

--- src/reducers/ux.js
+++ src/reducers/ux.js
@@ -1,8 +1,9 @@
-import { SET_MAIN_ONBOARDING_SEEN } from '../actions/types';
+import { SET_IS_MOBILE, SET_MAIN_ONBOARDING_SEEN } from '../actions/types';
 
 const defaultState = {
   mainOnboardingSeen: false,
   raceOnboardingSeen: false,
+  isMobile: true,
 };
 
 export default function reducer(state = defaultState, action) {
@@ -14,10 +15,19 @@ export default function reducer(state = defaultState, action) {
     };
   }
 
+  if (action.type === SET_IS_MOBILE) {
+    const { isMobile } = action;
+    return {
+      ...state,
+      isMobile,
+    };
+  }
+
   return state;
 }
 
 export const selectors = {
   getMainOnboardingSeen: state => state.mainOnboardingSeen,
   getRaceOnboardingSeen: state => state.raceOnboardingSeen,
+  getIsMobile: state => state.isMobile,
 };
